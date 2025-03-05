4377d6af0c73c8b9d99ab34dfada6eb0cbac6a14
coramdeo59 <melkamuelias59@gmail.com>
2025-03-05 21:20:42 +0300

    generics

--- src/01-generics-intro/04-multiple-generics-per-object.problem.ts
+++ src/01-generics-intro/04-multiple-generics-per-object.problem.ts
@@ -1,12 +1,20 @@
 import { expect, it } from "vitest";
 import { Equal, Expect } from "../helpers/type-utils";
 
-const returnBothOfWhatIPassIn = (params: { a: unknown; b: unknown }) => {
+// const returnBothOfWhatIPassIn = <A extends { a: any, b: any}>(params: A) => {
+//   return {
+//     first: params.a,
+//     second: params.b,
+//   };
+// };
+
+const returnBothOfWhatIPassIn = <A, B>(params: { a: A, b: B }) => {
   return {
     first: params.a,
-    second: params.b,
-  };
-};
+    second: params.b
+  }
+}
+
 
 it("Should return an object where a -> first and b -> second", () => {
   const result = returnBothOfWhatIPassIn({
